9439a8521a309386cf1c826ee77d043b2253c7f9
Jose Alvarez <josealvarezvivar@gmail.com>
2021-02-23 18:25:41 +0100

    creating the grid sistem and pkmn cards

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './fonts/roboto/roboto.ttf';
+import './fonts/tech_mono/tech_mono.ttf';
+
+import './styles/common/reset.css';
+import './styles/common/base.css';
+import './styles/site.css';
